1a576d456276a2d605414cd323bcb9f027b230b6
fazal-10 <fazalmansuri08@gmail.com>
2023-07-26 22:33:22 +0530

    comments added

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -1,21 +1,27 @@
+// Import required modules and packages
 const mongoose = require('mongoose');
 const shortId = require('shortid');
 
+// Define the Schema for the ShortUrl model
 const shortUrlSchema = new mongoose.Schema({
-    full : {
+    // Full URL field, required for each ShortUrl document
+    full: {
         type: String,
         required: true
     },
-    short : {
+    // Short URL field, required for each ShortUrl document, and generated using shortid package
+    short: {
         type: String,
-        required:true,
-        default: shortId.generate
+        required: true,
+        default: shortId.generate // Default value is generated using shortid package
     },
-    clicks : {
+    // Clicks field to store the number of times the short URL has been clicked, default is 0
+    clicks: {
         type: Number,
         required: true,
         default: 0
     }
-})
+});
 
-module.exports = mongoose.model('ShortUrl',shortUrlSchema);
+// Export the ShortUrl model with the defined schema
+module.exports = mongoose.model('ShortUrl', shortUrlSchema);
--- server.js
+++ server.js
@@ -1,34 +1,52 @@
+// Import required modules and packages
 const express = require('express');
 const mongoose = require('mongoose');
-const ShortUrl = require('./models/shortUrl');
+const ShortUrl = require('./models/shortUrl'); // Assuming the model is defined in 'models/shortUrl.js'
 var app = express();
 
+// Connect to MongoDB database
 mongoose.connect('mongodb://localhost/urlShortener', {
-    useNewUrlParser: true, useUnifiedTopology: true
-})
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
+// Set the view engine to use EJS templates
 app.set('view engine', 'ejs');
+
+// Middleware to parse the request body
 app.use(express.urlencoded({ extended: false }));
 
+// Route to display the list of shortened URLs
 app.get('/', async (req, res) => {
+    // Fetch all short URLs from the database
     const shortUrls = await ShortUrl.find();
-    res.render('index',{shortUrls:shortUrls});
-})
+    // Render the 'index' view and pass the list of short URLs to the template
+    res.render('index', { shortUrls: shortUrls });
+});
 
-app.post('/shortUrls',async (req, res) => {
+// Route to create a new short URL
+app.post('/shortUrls', async (req, res) => {
+    // Create a new ShortUrl document in the database with the full URL provided in the request body
     await ShortUrl.create({ full: req.body.fullUrl });
+    // Redirect back to the homepage after creating the short URL
     res.redirect('/');
 });
 
-app.get('/:shortUrl',async (req,res)=>{
-    const shortUrl = await ShortUrl.findOne({short : req.params.shortUrl});
+// Route to handle short URL redirection
+app.get('/:shortUrl', async (req, res) => {
+    // Find the corresponding short URL document in the database based on the provided short code
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
 
-    if(shortUrl == null) return res.sendStatus(404);
+    // If the short URL does not exist in the database, return a 404 Not Found status
+    if (shortUrl == null) return res.sendStatus(404);
 
+    // Increment the clicks count for the short URL and save the updated document in the database
     shortUrl.clicks++;
     shortUrl.save();
 
-    res.redirect(shortUrl.full);    //redirectiong user to full url by clicking on short url
-})
+    // Redirect the user to the original (full) URL by clicking on the short URL
+    res.redirect(shortUrl.full);
+});
 
+// Start the server and listen on the specified port or a default port 5000
 app.listen(process.env.PORT || 5000);
